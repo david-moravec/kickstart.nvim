7dc41ef0ad826b8959d5a4ec597e0856a1bb186b
David Moravec <moravec@numsolution.cz>
2023-12-04 09:40:40 +0100

    Trim comments clen up

--- init.lua
+++ init.lua
@@ -1,51 +1,6 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-
-  And then you can explore or search through `:help lua-guide`
-  - https://neovim.io/doc/user/lua-guide.html
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.python3_host_prog = 'E:/Programy/Python310/python.exe'
--- C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.3056.0_x64__qbz5n2kfra8p0
--- C:\Users\moravec\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0
 vim.wo.relativenumber = true
 
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -69,19 +24,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and "; " or ":") .. vim.env.PATH
 
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
 require('lazy').setup({
-  -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
   'sindrets/diffview.nvim',
+  require 'custom.plugins.lazygit',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
@@ -240,39 +189,8 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- {
-  --   'stevearc/conform.nvim',
-  --   opts = {},
-  --   event = { "BufReadPre", "BufNewFile" },
-  --   config = function()
-  --     local conform = require("conform")
-  --
-  --     conform.setup({
-  --       formatters_by_ft = {
-  --         python = { "prettier", "black" },
-  --       },
-  --       format_on_save = {
-  --         -- These options will be passed to conform.format()
-  --         timeout_ms = 500,
-  --         lsp_fallback = true,
-  --       },
-  --     })
-  --   end,
-  -- },
-
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
   require 'kickstart.plugins.debug',
-  require 'custom.plugins.lazygit',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
 }, {})
 
 -- [[ Setting options ]]
